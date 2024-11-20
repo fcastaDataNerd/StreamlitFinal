0c97b60704e438a13e3012875109eb7ae843aa80
Franco Castagliuolo <168364381+fcastaDataNerd@users.noreply.github.com>
2024-11-19 21:37:46 -0500

    Update Fantasy.py

--- Fantasy.py
+++ Fantasy.py
@@ -7,11 +7,6 @@ st.header("Greeting, gentlemen. Explore your tanking scenarios below (except for
 
 import numpy as np
 import pandas as pd
-pd.set_option('display.max_columns', None)
-# Load data and historical scores
-scores = pd.read_excel("Fantasy.xlsx", sheet_name="ScoringData")
-records = pd.read_excel("Fantasy.xlsx", sheet_name="Records")
-schedule = pd.read_excel("Fantasy.xlsx", sheet_name="Schedule")
-playoffs = pd.read_excel("Fantasy.xlsx", sheet_name="Playoffs")
+
 
 st.subheader("Week 12")
