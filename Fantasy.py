2f1b89d1dc4fb7be60db4e7a408465d9b1d29e39
Franco Castagliuolo <168364381+fcastaDataNerd@users.noreply.github.com>
2024-11-19 20:57:35 -0500

    Update Fantasy.py

--- Fantasy.py
+++ Fantasy.py
@@ -4,3 +4,14 @@ import streamlit as st
 st.title("ROS Playoff Scenarios")
 
 st.header("Greeting, gentlemen. Explore your tanking scenarios below (except for FS)")
+
+import numpy as np
+import pandas as pd
+pd.set_option('display.max_columns', None)
+# Load data and historical scores
+scores = pd.read_excel("C:\\Users\\Franco Castagliuolo\\OneDrive - Bentley University\\Fantasy.xlsx", sheet_name="ScoringData")
+records = pd.read_excel("C:\\Users\\Franco Castagliuolo\\OneDrive - Bentley University\\Fantasy.xlsx", sheet_name="Records")
+schedule = pd.read_excel("C:\\Users\\Franco Castagliuolo\\OneDrive - Bentley University\\Fantasy.xlsx", sheet_name="Schedule")
+playoffs = pd.read_excel("C:\\Users\\Franco Castagliuolo\\OneDrive - Bentley University\\Fantasy.xlsx", sheet_name="Playoffs")
+
+st.subheader("Week 12")
